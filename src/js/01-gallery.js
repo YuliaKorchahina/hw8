f8e37f722a7fdec1d54c40e7bd1cb0a044d12f93
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-14 17:31:40 +0200

    vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const list = galleryItems.reduce(
+  (acc, { preview, description, original }) =>
+    acc +
+    `<a class="gallery__link" href='${original}'>
+    <img class="gallery__image" 
+    src='${preview}'
+    alt='${description}'/>
+    </a>`,
+  ''
+);
+
+galleryEl.insertAdjacentHTML('afterbegin', list);
+
+galleryEl.addEventListener('click', onClick);
+function onClick(event) {
+  event.preventDefault();
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,5 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
