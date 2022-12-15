ff5dcf8eeb8dcb7529b383034534a6288840e354
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-15 23:36:51 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new Player(iframeEl);
+
+const setDataToLocalStorage = throttle((storageName, data) => {
+  localStorage.setItem(storageName, data);
+}, 1000);
+
+const getDataToLocalStorage = storageName => {
+  return localStorage.getItem(storageName) ?? 0;
+};
+
+player.setCurrentTime(getDataToLocalStorage(VIDEOPLAYER_CURRENT_TIME));
+
+player.on('timeupdate', data => {
+  setDataToLocalStorage(VIDEOPLAYER_CURRENT_TIME, data.seconds);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+const inputEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
 
+const handler = event => {
+  event.preventDefault();
+  localStorage.setItem('inputEl', 'messageEl.value');
+};
+inputEl.addEventListener('click', handler);
+console.log(messageEl.value);
