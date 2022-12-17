f9db0b11a4b6cd924850076db37bbaf8ee62a0b0
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-17 19:25:12 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,55 @@
-const inputEl = document.querySelector('input');
-const messageEl = document.querySelector('textarea');
+const KEY_LOCAL_STORAGE = 'feedback-form-state';
 
-const handler = event => {
-  event.preventDefault();
-  localStorage.setItem('inputEl', 'messageEl.value');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+
+const setDataToLokalStorage = (key, value) => {
+  localStorage.setItem(key, value);
 };
-inputEl.addEventListener('click', handler);
-console.log(messageEl.value);
+
+const getDateFromLocalStorage = key => {
+  return localStorage.getItem(key);
+};
+
+const handle = event => {
+  const name = event.currentTarget.name;
+  const value = event.currentTarget.value;
+
+  const data = JSON.parse(getDateFromLocalStorage(KEY_LOCAL_STORAGE));
+
+  setDataToLokalStorage(
+    KEY_LOCAL_STORAGE,
+    JSON.stringify({
+      ...data,
+      [name]: value,
+    })
+  );
+};
+
+const setDataToFildForm = data => {
+  if (data) {
+    email.value = data.email ?? '';
+    message.value = data.message ?? '';
+  }
+};
+
+const removeItem = key => {
+  localStorage.removeItem(key);
+};
+
+const resetDataForm = () => {
+  removeItem(KEY_LOCAL_STORAGE);
+};
+
+email.addEventListener('input', handle);
+message.addEventListener('input', handle);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  form.submit();
+  form.reset();
+  resetDataForm();
+});
+
+const data = JSON.parse(getDateFromLocalStorage(KEY_LOCAL_STORAGE));
+setDataToFildForm(data);
