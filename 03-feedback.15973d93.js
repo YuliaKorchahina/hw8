f3b7da57b30e4a3500cc26fef51d2e9cc85d62f1
YuliaKorchahina <106467729+YuliaKorchahina@users.noreply.github.com>
2022-12-17 17:24:43 +0000

    Deploying to gh-pages from @ YuliaKorchahina/hw8@f9db0b11a4b6cd924850076db37bbaf8ee62a0b0 🚀

--- 03-feedback.15973d93.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("input"),t=document.querySelector("textarea");e.addEventListener("click",(function(e){e.preventDefault(),localStorage.setItem("inputEl","messageEl.value")})),console.log(t.value)}();
-//# sourceMappingURL=03-feedback.15973d93.js.map
--- 03-feedback.f9f3a56e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("input"),t=document.querySelector("textarea");e.addEventListener("click",(e=>{e.preventDefault(),localStorage.setItem("inputEl","messageEl.value")})),console.log(t.value);
-//# sourceMappingURL=03-feedback.f9f3a56e.js.map
