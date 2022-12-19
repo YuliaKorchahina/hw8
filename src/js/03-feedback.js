f18334af777e236eb56b4de9ee505823e663587a
YuliaKorchahina <ruda_nokia@ukr.net>
2022-12-19 18:00:24 +0200

    add throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,14 @@
+import throttle from 'lodash.throttle';
+
 const KEY_LOCAL_STORAGE = 'feedback-form-state';
 
+const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea');
-const form = document.querySelector('.feedback-form');
 
-const setDataToLokalStorage = (key, value) => {
+const setDataToLokalStorage = throttle((key, value) => {
   localStorage.setItem(key, value);
-};
+}, 500);
 
 const getDateFromLocalStorage = key => {
   return localStorage.getItem(key);
@@ -42,12 +44,20 @@ const resetDataForm = () => {
   removeItem(KEY_LOCAL_STORAGE);
 };
 
+const handleSubmit = event => {
+  const data = JSON.parse(getDateFromLocalStorage(KEY_LOCAL_STORAGE));
+  event.preventDefault();
+  event.stopPropagation();
+  form.reset();
+};
+
 email.addEventListener('input', handle);
 message.addEventListener('input', handle);
 form.addEventListener('submit', event => {
   event.preventDefault();
-  form.submit();
-  form.reset();
+  event.stopPropagation();
+  handleSubmit(event);
+  console.log(data);
   resetDataForm();
 });
 
